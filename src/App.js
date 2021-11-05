cf7b9d76116e75402da9d97599522d52693ad8ad
mrandolph83 <mrandolph83@gmail.com>
2021-11-04 23:29:11 -0400

    Add fetch call to toy collection, update local state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './App.css';
-import './db.json' from ./db.json 
+// import './db.json' from ./db.json 
 import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
 
 class App extends React.Component{
+state = {
+  display: false
+  toys: []
+}
 
-  state = {
-    display: false
-  }
+componentDidMount() {
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      this.setState({
+        toys: data.toys,
+      });
+    });
+}
+}
+
+  // 1 - ComponentDidMount and fetch call to toys. 
+  // 2 - Data/props flow for toys
+  // 3 - add a create function for toys
+  // 4 - add a delete function for toys
+  // 5 - undersand json-server
+
 
   handleClick = () => {
     let newBoolean = !this.state.display
